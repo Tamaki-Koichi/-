667187385e916756972ac62c42fbe53c7c44cf7c
Tamaki-Koichi <68366168+Tamaki-Koichi@users.noreply.github.com>
2021-01-20 21:09:33 +0900

    Add files via upload

---
+++ php/chapter3/welcome-html.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+      <head>
+      <meta charset="UTF-8">
+      <title>PHP Sample Programs</title>
+      <link rel="stylesheet" href="../style.css">
+      <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+            <?php 
+                  echo 'いらっしゃいませ！';
+            ?>
+      </body>
+</html>
---
+++ php/chapter3/welcome.php
@@ -0,0 +1,5 @@
+<?php
+echo 'welcome'; 
+echo 'to';
+echo 'PHP';
+?>
